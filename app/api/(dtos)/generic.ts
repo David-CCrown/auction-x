c4cc6f221b8ec183dc90c13a40d4c25b21991eef
AdemolaSam <oduntanade2721@gmail.com>
2025-05-13 18:23:21 +0100

    Feat: auction page

---
+++ app/api/(dtos)/generic.ts
@@ -0,0 +1,2 @@
+export const ErrorMessage =
+  "An error occured! Please try again or contact support if error persists";
--- app/api/auction/route.ts
+++ app/api/auction/route.ts
@@ -1,5 +1,29 @@
+import supabase from "@/config/supabase";
 import { NextResponse } from "next/server";
+import { ErrorMessage } from "../(dtos)/generic";
 
 export async function GET(req: Request) {
-  return NextResponse.json({ message: "Welcome to AuctionX" });
+  try {
+    const { data, error, status } = await supabase.from("auction").select("*");
+    if (error) {
+      return NextResponse.json({ error }, { status });
+    }
+    return NextResponse.json({ data });
+  } catch (err) {
+    return NextResponse.json({ error: err, message: ErrorMessage });
+  }
+}
+
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  const { data, error, status } = await supabase
+    .from("auction")
+    .insert([{ ...body }])
+    .select();
+
+  if (error) {
+    return NextResponse.json({ error }, { status });
+  }
+  return { data };
 }
